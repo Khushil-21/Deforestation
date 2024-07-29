aec4b4c55abc5962a1794c5e35185da4706a0752
Khushil-21 <khushilshah2105@gmail.com>
2024-07-29 22:56:06 +0530

    for local dev

--- backend/index.py
+++ backend/index.py
@@ -7,3 +7,7 @@ app = FastAPI()
 @app.get("/")
 def hello_world():
     return {"message": "Hello, World!"}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    uvicorn.run(app, host="localhost", port=port)
