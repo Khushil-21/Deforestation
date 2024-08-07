24b8290e6e0db9d9983870f54c3d401361133083
Khushil-21 <khushilshah2105@gmail.com>
2024-08-07 22:33:35 +0530

    Entry Page Animation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import MainComponent from './components/MainComponent';
+import EntryComponent from './components/EntryComponent';
 
 function App() {
+  const [showMainComponent, setShowMainComponent] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowMainComponent(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
-    <MainComponent />
+    <>
+      {showMainComponent ? <MainComponent /> : <EntryComponent />}
+    </>
   );
 }
 
---
+++ frontend/src/utils/StaticData.js
@@ -0,0 +1,9 @@
+export const CompanyName = "Canopy Watch";
+export const CompanyTagline = "Unveiling Earth's Green History: Track Deforestation and Empower Nature's Future";
+
+export const MainPageLoadingDelay = 4000;
+
+export const EntryPageDuration = 2;
+export const TitleAnimationDuration = 2;
+export const TaglineAnimationDuration = 2;
+
